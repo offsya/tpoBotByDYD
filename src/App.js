67e2827f434b6cd13579dd348d0bc142a40556a5
Yura <idblablobla@gmail.com>
2022-11-28 17:13:50 +0300

    fr123

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/Items";
 import Categories from "./components/Categories";
-import categories from "./components/Categories";
 import ShowFullItem from "./components/ShowFullItem";
 
 function App() {
